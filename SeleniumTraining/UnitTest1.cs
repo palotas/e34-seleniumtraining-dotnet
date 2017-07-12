3337dbc5dcfda9f3a4f8b78b2be447023153daf5
palotas <michael.palotas@element34.com>
2017-07-12 22:51:10 +0200

    changes method name

--- SeleniumTraining/UnitTest1.cs
+++ SeleniumTraining/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace SeleniumTraining
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void firstSeleniumTest()
         {
             IWebDriver driver = new ChromeDriver();
             try
