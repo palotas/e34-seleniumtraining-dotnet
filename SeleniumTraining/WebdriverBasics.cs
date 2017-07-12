51cff1ede6544f5447bde1c400fabf41a3548d53
palotas <michael.palotas@element34.com>
2017-07-12 22:53:14 +0200

    rename class

--- SeleniumTraining/WebdriverBasics.cs
+++ SeleniumTraining/WebdriverBasics.cs
@@ -6,7 +6,7 @@ using OpenQA.Selenium.Chrome;
 namespace SeleniumTraining
 {
     [TestClass]
-    public class UnitTest1
+    public class WebdriverBasics
     {
         [TestMethod]
         public void firstSeleniumTest()
