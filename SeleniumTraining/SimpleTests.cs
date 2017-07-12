3ba57df3d521f76ca9fce60b6407a2206b760371
palotas <michael.palotas@element34.com>
2017-07-12 23:00:04 +0200

    adding more tests

---
+++ SeleniumTraining/SimpleTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumTraining
+{
+    [TestClass]
+    public class SimpleTests
+    {
+        [TestMethod]
+        public void firstTest()
+        {
+            int a = 1;
+            int b = 2;
+            int c = a - b;
+
+            Console.Write("C = " + c);
+            Assert.IsTrue(c == 3);
+        }
+    }
+}
