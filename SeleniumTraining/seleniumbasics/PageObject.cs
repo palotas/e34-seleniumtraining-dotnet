d7977a2e36b2ce03273d085f62e220439bbc2064
palotas <michael.palotas@element34.com>
2017-07-14 20:11:03 +0200

    adding page object

---
+++ SeleniumTraining/seleniumbasics/PageObject.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace SeleniumTraining.seleniumbasics
+{
+    class PageObject
+    {
+        private readonly IWebDriver driver;
+        private readonly string url = @"http://www.bing.com/";
+
+        public PageObject(IWebDriver browser)
+        {
+            this.driver = browser;
+            PageFactory.InitElements(browser, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "sb_form_q")]
+        public IWebElement SearchBox { get; set; }
+
+        [FindsBy(How = How.Id, Using = "sb_form_go")]
+        public IWebElement GoButton { get; set; }
+
+        [FindsBy(How = How.Id, Using = "b_tween")]
+        public IWebElement ResultsCountDiv { get; set; }
+
+        public void Navigate()
+        {
+            this.driver.Navigate().GoToUrl(this.url);
+        }
+
+        public void Search(string textToType)
+        {
+            this.SearchBox.Clear();
+            this.SearchBox.SendKeys(textToType);
+            this.GoButton.Click();
+        }
+
+        public void ValidateResultsCount(string expectedCount)
+        {
+            Assert.IsTrue(this.ResultsCountDiv.Text.Contains(expectedCount),
+        "The results DIV doesn't contains the specified text.");
+        }
+
+    }
+}
---
+++ SeleniumTraining/seleniumbasics/PageObjectTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTraining.seleniumbasics
+{
+    [TestClass]
+    public class PageObjectTest
+    {
+        public IWebDriver Driver { get; set; }
+        public WebDriverWait Wait { get; set; }
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            this.Driver = new ChromeDriver();
+            this.Wait = new WebDriverWait(this.Driver, TimeSpan.FromSeconds(30));
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            this.Driver.Quit();
+        }
+
+        [TestMethod]
+        public void SearchTextInBing_First()
+        {
+            PageObject bingMainPage = new PageObject(this.Driver);
+            bingMainPage.Navigate();
+            bingMainPage.Search("Automate The Planet");
+            bingMainPage.ValidateResultsCount("264,000 RESULTS");
+        }
+    }
+
+    
+
+}
