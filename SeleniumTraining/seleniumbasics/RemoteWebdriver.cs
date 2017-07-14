5b4fa082536900cb5cd4c8b03a2127741c52d8ba
palotas <michael.palotas@element34.com>
2017-07-14 20:00:10 +0200

    added remotewebdriver

---
+++ SeleniumTraining/seleniumbasics/RemoteWebdriver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace SeleniumTraining.seleniumbasics
+{
+    [TestClass]
+    public class RemoteWebdriver
+    {
+        [TestMethod]
+        public void remoteWebdriver()
+        {
+            RemoteWebDriver driver;
+            DesiredCapabilities capabilities = DesiredCapabilities.Chrome();
+            driver = new RemoteWebDriver(capabilities);
+            driver.Navigate().GoToUrl("http://www.google.com");
+            Console.WriteLine(driver.Title);            
+            driver.Quit();
+        }
+    }
+}
